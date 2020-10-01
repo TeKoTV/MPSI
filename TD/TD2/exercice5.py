30291a10b142308719262f3a38064232da97caff
TeKo <emilien797@gmail.com>
2020-10-01 14:33:52 +0200

    Changed floor to round

--- TD/TD2/exercice5.py
+++ TD/TD2/exercice5.py
@@ -1,10 +1,8 @@
-from math import floor
-
 t = float(input("Donnez votre taille :"))
 m = int(input("Donnez votre poids :"))
 
 imc = m/(t**2)
 
-imc = floor(imc * 100) / 100
+imc = round(imc * 100) / 100
 
 print("Vous mesurez ", t, " et pesez ", m, "kg et votre IMC vaut", imc)
