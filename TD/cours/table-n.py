82ecfb86e79f18fb801bbf6475374151b9f0d661
TeKo <emilien797@gmail.com>
2020-10-08 14:31:01 +0200

    def mul

--- TD/cours/table-n.py
+++ TD/cours/table-n.py
@@ -3,8 +3,11 @@ n = eval(input("N = "))
 
 print("Table des n :")
 
-# Boucle de 1 à 10
-for i in range (1,10):
-    
-    # Afficher le résultat
-    print(n,"x", i, " = ", n*i)
+def mul(n):
+    # Boucle de 1 à 10
+    for i in range (1,11):
+
+        # Afficher le résultat
+        print(n,"x", i, " = ", n*i)
+
+mul(n)
